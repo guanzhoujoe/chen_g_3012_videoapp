b79e280e75339a87dae2ca97605c481d6cd9e47e
guanzhoujoe <guanzhou1009@gmail.com>
2018-04-19 22:18:49 -0400

    final video app

--- routes/kid.js
+++ routes/kid.js
@@ -3,14 +3,12 @@ var videoController = require('../controllers/videoAppController');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', videoController.get_homepage);
-
-router.get('/parent', videoController.get_all_movies);
+// router.get('/', videoController.get_all_movies);
 
 router.get('/kid', videoController.get_all_movies_kid);
-
-router.get('/movies/:id/:movie', videoController.get_one_movie );
-
-router.post('/api', videoController.post_new_review);
+//
+// router.get('/movies/:id/:movie', videoController.get_one_movie );
+//
+// router.post('/api', videoController.post_new_review);
 
 module.exports = router;
